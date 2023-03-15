7ba6a495a570cde78cbf72313893477623f51391
zpw <panwei.zhou@yingxiong.com>
2023-03-15 10:26:16 +0800

    feat: vue2

---
+++ build/webpack.base.js
@@ -0,0 +1,53 @@
+const path = require('path')
+const {VueLoaderPlugin} = require('vue-loader')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { compilerOptions } = require('vue-template-compiler')
+module.exports = {
+  mode: 'development',
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          compilerOptions: {
+            ...compilerOptions,
+            preserveWhitespace: false
+          }
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['vue-style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ],
+  devServer: {
+    hot: true,
+    open: true,
+    port: "9527",
+  },
+}
