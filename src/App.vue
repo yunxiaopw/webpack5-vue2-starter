d76e36249c5b59154ccb5ae826f9230dd5ede0cd
zpw <panwei.zhou@yingxiong.com>
2023-03-28 16:55:11 +0800

    fi: 验证是否支持 chrome 59

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
 <template>
-  <div>home我王薇薇asdaasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdsd a</div>
+  <div>
+    home
+    <div>{{ result }}</div>
+    <div>{{ result2 }}</div>
+  </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      data: [111, 222, 333],
+      result: [],
+      result2: {},
+    };
+  },
   methods: {
-    handle3d() {},
+    handle3d() {
+      const a = ['asd', 'asdasd', 'fgdfgdfg'];
+      const b = this.data.concat(a);
+      this.result = b;
+      const c = { a: 1, c: 2 };
+      const d = { ...c, f: 'f' };
+      this.result2 = d;
+    },
   },
   mounted() {
     this.handle3d();
